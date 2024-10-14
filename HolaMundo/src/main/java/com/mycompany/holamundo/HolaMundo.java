466b9398af8a76e9b2be5fb0e2a350aeb5f23c30
pedro vallejos <pepechulo77@gmail.com>
2024-10-14 10:43:23 -0300

    estoy probando

---
+++ HolaMundo/src/main/java/com/mycompany/holamundo/HolaMundo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author pedro
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Mi primeras lineas en java");
+    }
+}
